bb4a0023b693f5cb47a5e8719b8a4737825c000c
Christian Ruigrok <christian@ruigrok.info>
2018-03-27 09:05:31 +0200

    Move dashboard item to bottom of sidebar

--- src/components/content/Sidebar.jsx
+++ src/components/content/Sidebar.jsx
@@ -6,14 +6,14 @@ import { MapsIcon, ListIcon, DashboardIcon } from '../util/static/icons';
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <NavLink className="sidebar__item" to="/dashboard">
-        <DashboardIcon />
-      </NavLink>
       <NavLink className="sidebar__item" to="/kaart">
         <MapsIcon />
       </NavLink>
       <NavLink className="sidebar__item" to="/lijst">
         <ListIcon />
+      </NavLink>
+      <NavLink className="sidebar__item" to="/dashboard">
+        <DashboardIcon />
       </NavLink>
     </div>
   );
